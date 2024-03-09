ee3c0b5956cb7e212c455f9cd285b59ed5377372
Farkhadov95 <farkhadov950405@gmail.com>
2024-03-09 21:30:06 +0500

    Added token jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
 const User = require("../models/user");
@@ -11,8 +12,8 @@ router.post("/", async (req, res) => {
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).send("Invalid email or password");
 
-    // const token = user.generateAuthToken();
-    res.send(true);
+    const token = jwt.sign({ _id: user._id }, 'jwtPrivateKey');
+    res.send(token);
 });
 
 module.exports = router;
