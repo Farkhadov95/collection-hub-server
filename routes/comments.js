22b1d80ec6b60bf9886b76237de41d9cda992f4d
Farkhadov95 <farkhadov950405@gmail.com>
2024-03-18 20:23:49 +0500

    Updated Get method

--- routes/comments.js
+++ routes/comments.js
@@ -6,17 +6,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/:itemID', async (req, res) => {
-    console.log(req.params.itemID);
+    const itemID = req.params.itemID;
     try {
-        const item = await Item.findById(req.params.itemID);
-        if (!item) res.status(404).json({ error: 'Item not found' });
-
-        const commentIDs = item.commentIDs;
-        const comments = await Promise.all(commentIDs.map(async (commentID) => {
-            const comment = await Comment.findById(commentID);
-            return comment;
-        }));
-
+        const comments = await Comment.find({ itemID: itemID })
         res.send(comments);
     } catch (error) {
         console.error('Error occurred while processing the request:', error);
