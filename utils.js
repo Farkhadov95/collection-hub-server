d6056d66032eaa04be4253d999f60f9a0d6f6eb5
Farkhadov95 <farkhadov950405@gmail.com>
2024-03-22 12:56:47 +0500

    added UnionWith

--- utils.js
+++ utils.js
@@ -2,6 +2,8 @@ const mongodb = require('mongodb');
 
 const MONGODB_DATABASE = 'test'
 const MONGODB_COLLECTION = 'collections'
+const MONGODB_ITEMS_COLLECTION = 'items'
+const MONGODB_COMMENTS_COLLECTION = 'comments'
 
 const MONGODB_HOST = process.env.MONGODB_URI;
 const MONGODB_USER = process.env.MONGODB_USER
@@ -11,4 +13,4 @@ const mongoClient = new mongodb.MongoClient(MONGODB_HOST, {
     auth: { username: MONGODB_USER, password: MONGODB_PASS },
 })
 
-module.exports = { MONGODB_COLLECTION, MONGODB_DATABASE, mongoClient };
+module.exports = { MONGODB_COLLECTION, MONGODB_DATABASE, mongoClient, MONGODB_ITEMS_COLLECTION, MONGODB_COMMENTS_COLLECTION };
